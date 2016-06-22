153eb7ff785c0fb8b8790560e5743bd84f6bc98c
frank <loumingjie@foxmail.com>
2016-06-22 19:25:18 +0800

    fix

---
+++ build/index/index.js
@@ -0,0 +1,3 @@
+
+define('index/index',function(require,exports,moudle){var instance={init:function(){alert(1);var render=new THREE.WebGLRenderer();render.setSize(400,300);document.querySelector('body').appendChild(render.domElement);render.setClearColor(0x000000);var scene=new THREE.Scene();var camera=new THREE.PerspectiveCamera(45,4/3,1,1000);camera.position.set(0,0,5);scene.add(camera);var cube=new THREE.Mesh(new THREE.CubeGeometry(1,2,3),new THREE.MeshBasicMaterial({color:0xff0000}));scene.add(cube);render.render(scene,camera);}}
+instance.init();})
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var 
+	gulp   = require('gulp'),
+	del    = require('del'),
+	rename = require('gulp-rename'),
+	less   = require('gulp-less'),
+	cssmin = require('gulp-cssmin'),
+	prefix = require('gulp-autoprefixer'),
+	jsmin  = require('gulp-jsmin'),
+	imgmin = require('gulp-imagemin'),
+	juicer = require('gulp-juicer-js'),
+	server = require('gulp-devserver'),
+	essi   = require('essi'),
+	yargs  = require('yargs').argv,
+	htmlmin = require('gulp-htmlmin');
+
+/**
+ * 清除bulid目录
+ */
+gulp.task('clean', function() {
+    del(['bulid']);
+});
+
+/**
+ * CSS任务,less,加前缀,压缩
+ */
+gulp.task('css', function() {
+    gulp.src('src/**/*.less')
+      .pipe(less())
+      .pipe(prefix())
+      .pipe(cssmin())
+      .pipe(rename(function( path ){
+      	path.extname = '.css';
+      }))
+      .pipe(gulp.dest('bulid/'));
+      console.log('css编译成功');
+});
+
+/**
+ * js任务
+ */
+gulp.task('js', function() {
+	gulp.src('src/**/*.js')
+	  .pipe(jsmin())
+	  .pipe(gulp.dest('build/'));    
+	  console.log('js编译成功');
+});
+
+ /**
+  * html 压缩
+  */
+gulp.task('html', function() {
+    gulp.src('src/**/*.html')
+      .pipe(htmlmin(''))
+      .pipe(gulp.dest('build/'));
+      console.log('html编译成功');
+});
+
+gulp.task('default',['clean', 'css', 'js', 'html']);
